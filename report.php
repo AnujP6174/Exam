f2001a9ca695bdc0abf81490d41213e973c5ea1f
Anuj Panchal <53812609+AnujP6174@users.noreply.github.com>
2021-08-02 16:01:49 +0530

    commit

--- report.php
+++ report.php
@@ -1,15 +1,21 @@
 <?php
-    // include('dash.php');
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    session_start();
+    $conn = mysqli_connect("localhost", "root", "", "rbeitest_db");
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Progress Report</title>
 </head>
+
 <body>
-    
+
 </body>
+
 </html>

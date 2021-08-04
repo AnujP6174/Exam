e9750c4cb43a8ba619f41874d94c461dc60763bb
Anuj Panchal <53812609+AnujP6174@users.noreply.github.com>
2021-08-04 21:45:31 +0530

    init

--- report.php
+++ report.php
@@ -44,33 +44,38 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
                 $result = mysqli_query($conn, $sql);
                 $count = mysqli_num_rows($result);
                 $exam_id_array = array();
-                $value=$exam_id_array;
-                $marks_query = "SELECT * FROM `rb_studentexamresult_tb` WHERE studentid='$user_id' AND testid='$value'";
-                $result1 = mysqli_query($conn, $marks_query);
-                $count1 = mysqli_num_rows($result1);
-                $total_marks = 0;
-                while ($row = mysqli_fetch_array($result1)) {
-                    $total_marks += $row[6];
-                }
+                $exam_marks = array();
+                $exam_title = array();
+
                 while ($TitleRow = mysqli_fetch_array($result)) {
                     $titl = $TitleRow[1] . "_" . $TitleRow[0];
                     $IdRow = substr($titl, strpos($titl, '_', 0) + 1, strlen($titl));
                     array_push($exam_id_array, $IdRow);
-                    echo "<tr class='table-primary'><td>$titl</td>
-                    <td>Given</td>
-                    <td>$total_marks</td></tr>";
+                    array_push($exam_title,$titl);
                 }
-                // <!-- List of Exams ends -->
-                // <!-- Score starts -->
 
                 foreach ($exam_id_array as $value) {
-                    while ($TitleRow = mysqli_fetch_array($result1)) {
-                        $titl = $TitleRow[1] . "_" . $TitleRow[0];
-                        $IdRow = substr($titl, strpos($titl, '_', 0) + 1, strlen($titl));
-                        array_push($exam_id_array, $IdRow);
+                    $user_id = $_SESSION['id'];
+                    $marks_query = "SELECT * FROM `rb_studentexamresult_tb` WHERE studentid='$user_id' AND testid='$value'";
+                    $result1 = mysqli_query($conn, $marks_query);
+                    $count1 = mysqli_num_rows($result1);
+                    $total_marks = 0;
+                    while ($row = mysqli_fetch_array($result1)) {
+                        $total_marks += $row[6];
                     }
+                    $percent_marks = ($total_marks*100)/80;
+                    array_push($exam_marks, $percent_marks);
                 }
-
+                for ($i = 0; $i < count($exam_id_array); $i++) {
+                    echo "<tr><td>$exam_title[$i]</td>";
+                    if($exam_marks[$i]>0){
+                        echo "<td> Given </td>";
+                    }
+                    else{
+                        echo "<td> Not Given </td>";
+                    }
+                    echo "<td>$exam_marks[$i]%</td></tr>";
+                }
                 ?>
                 <!-- Score ends -->
             </tbody>

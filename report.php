bf1ed861f61a322e24107f79a89696615b186e51
Priyansh Gupta <44543275+Priyansh2906@users.noreply.github.com>
2021-08-06 13:29:28 +0530

    Update report.php

--- report.php
+++ report.php
@@ -55,11 +55,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     </nav>
     <!-- Navbar ends -->
     <!-- Table start -->
-    <div class="container my-4">
+    <div class="container my-3">
         <table class="table table-striped table-hover table-bordered" id="myTable">
             <thead class="table-dark">
                 <tr style="text-align:center">
-                    <th scope="col">Sr. No.</th>
+                    <th scope="col">Serial No.</th> 
                     <th scope="col">List of Test</th>
                     <th scope="col">Exam Status</th>
                     <th scope="col">Score</th>
@@ -69,28 +69,24 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
             <tbody>
                 <?php
                 // List of Exams starts
-                $uid = $_SESSION['id'];
+                $user_id = $_SESSION['id'];
                 $ExmClss = $_SESSION['class'];
                 $ExmClss = $ExmClss . '%';
                 $sql = "SELECT * FROM `rb_studentexam_tb` WHERE class LIKE '$ExmClss'";
                 $result = mysqli_query($conn, $sql);
                 $count = mysqli_num_rows($result);
+                $exam_serial_array = array();
                 $exam_id_array = array();
                 $exam_marks = array();
                 $exam_title = array();
-                // $status_array=array();
-                // $image=array();
 
-                // ------------------------------------
-                // Exam title fetch starts
                 while ($TitleRow = mysqli_fetch_array($result)) {
                     $titl = $TitleRow[1] . "_" . $TitleRow[0];
                     $IdRow = substr($titl, strpos($titl, '_', 0) + 1, strlen($titl));
                     array_push($exam_id_array, $IdRow);
                     array_push($exam_title, $titl);
                 }
-                // Exam title fetch ends
-                // ------------------------------------
+
                 foreach ($exam_id_array as $value) {
                     $user_id = $_SESSION['id'];
                     $marks_query = "SELECT * FROM `rb_studentexamresult_tb` WHERE studentid='$user_id' AND testid='$value'";
@@ -103,9 +99,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
                     $percent_marks = ($total_marks * 100) / 80;
                     array_push($exam_marks, $percent_marks);
                 }
-
                 for ($i = 0; $i < count($exam_id_array); $i++) {
+                    $i = $i+1;
+                    array_push($exam_serial_array,$i);
                     echo "<tr class='table-success' style='text-align:center'><td>$i</td>";
+                    $i = $i-1;
                     echo "<td>$exam_title[$i]</td>";
                     if ($exam_marks[$i] > 0) {
                         echo "<td> Given </td>";
@@ -113,32 +111,67 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
                         echo "<td> Exam Pending </td>";
                     }
                     echo "<td>$exam_marks[$i]%</td>";
-                    echo "<td><a href='solution.php'><input type='button' class='btn btn-success' id='butn' value='View Solution'></a></td></tr>";
+                    echo "<td><a href='https://www.rbeiset.com/packageexam/result.php?studentid=$user_id&testid=$exam_id_array[$i]&device=desktop'><input type='button' value='View Solution'></a></td></tr>";
                 }
                 ?>
                 <!-- Score ends -->
             </tbody>
         </table>
+        <center>
+            
+        <input type="button" id="view_chart_btn"  value="View Detailed Marks Chart">
+        
+        <div id="columnchart_material" style="width: 800px; height: 500px; display:none;"></div></center>
+        
     </div>
     <!-- Table ends -->
-    <form action="solution.php" method="POST">
-        <script src="jquery.min.js"></script>
-        <script>
-            $('.table tbody').on('click', '.btn', function() {
-                var currow = $(this).closest('tr');
-                var col1 = currow.find('td:eq(1)').text();
-                var p_id = col1.substr(col1.search('_') + 1, col1.length);
-                alert(p_id);
-            })
-        </script>;
-    </form>
     <!-- Datatables javascript start -->
     <script>
         $(document).ready(function() {
             $('#myTable').DataTable();
         });
     </script>
+
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['bar']});
+      google.charts.setOnLoadCallback(initialize);
+
+      function initialize () {
+        $("#view_chart_btn").click(function() {
+            document.getElementById("view_chart_btn").style.display = "none";
+            document.getElementById("columnchart_material").style.display = "block";
+            drawChart();
+            document.getElementById("columnchart_material").scrollIntoView();
+        });
+        }
+      var serial = JSON.parse('<?php echo json_encode($exam_serial_array); ?>');
+      var Marks = JSON.parse('<?php echo json_encode($exam_marks); ?>');
+      console.log(serial);
+      console.log(Marks);
+
+      function drawChart() {
+        // create DataTable
+        var data = new google.visualization.DataTable();
+        data.addColumn('number', 'Exam Subject Serial.');
+        data.addColumn('number', 'Marks');
+
+        // load data
+        for (var i = 0; i < Marks.length; i++) {
+            var row = [serial[i], Marks[i]];
+            data.addRow(row);
+        }
+        var options = {
+          chart: {
+        
+          }
+        };
+
+        var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+        chart.draw(data, google.charts.Bar.convertOptions(options));
+      }
+    </script>
+
     <!-- Datatables javascript ends -->
 </body>
-
 </html>

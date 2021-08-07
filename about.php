71ef419f9ddf167c46750a3a33c459d7b56731c7
Anuj Panchal <53812609+AnujP6174@users.noreply.github.com>
2021-08-07 14:07:12 +0530

    init

--- about.php
+++ about.php
@@ -12,6 +12,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="dashboard.css">
     <style>
         body {
             background: linear-gradient(120deg, #2980b9, #8e44ad);
@@ -20,6 +26,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 
         span {
             color: #ff8080;
+        }
+        p{
+            color: #ffcc99;
         }
     </style>
 </head>
@@ -45,50 +54,44 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         </div>
     </nav>
     <!-- Navbar ends -->
+    <center>
     <div class="container my-4">
-        <p><b>Name:</b> <?php
-                        $un = $_SESSION['username'];
-                        $sql = "SELECT * FROM `rb_user_tb` WHERE username='$un'";
-                        $result = mysqli_query($conn, $sql);
-                        while ($row = mysqli_fetch_assoc($result)) {
-                            echo $row['name'];
-                        }
-                        ?></p>
-        <p><b>Class:</b> <?php
+            <p><b>Name:</b> <?php
                             $un = $_SESSION['username'];
                             $sql = "SELECT * FROM `rb_user_tb` WHERE username='$un'";
                             $result = mysqli_query($conn, $sql);
                             while ($row = mysqli_fetch_assoc($result)) {
-                                echo $row['class'];
+                                echo $row['name'];
                             }
                             ?></p>
-        <p><b>Mobile:</b> <?php
-                            $un = $_SESSION['username'];
-                            $sql = "SELECT * FROM `rb_user_tb` WHERE username='$un'";
-                            $result = mysqli_query($conn, $sql);
-                            while ($row = mysqli_fetch_assoc($result)) {
-                                echo $row['mobile'];
-                            }
-                            ?></p>
-        <p><b>Email:</b> <?php
-                            $un = $_SESSION['username'];
-                            $sql = "SELECT * FROM `rb_user_tb` WHERE username='$un'";
-                            $result = mysqli_query($conn, $sql);
-                            while ($row = mysqli_fetch_assoc($result)) {
-                                echo $row['email'];
-                            }
-                            ?></p>
-    </div>
-    <!-- Optional JavaScript; choose one of the two! -->
-
+            <p><b>Class:</b> <?php
+                                $un = $_SESSION['username'];
+                                $sql = "SELECT * FROM `rb_user_tb` WHERE username='$un'";
+                                $result = mysqli_query($conn, $sql);
+                                while ($row = mysqli_fetch_assoc($result)) {
+                                    echo $row['class'];
+                                }
+                                ?></p>
+            <p><b>Mobile:</b> <?php
+                                $un = $_SESSION['username'];
+                                $sql = "SELECT * FROM `rb_user_tb` WHERE username='$un'";
+                                $result = mysqli_query($conn, $sql);
+                                while ($row = mysqli_fetch_assoc($result)) {
+                                    echo $row['mobile'];
+                                }
+                                ?></p>
+            <p><b>Email:</b> <?php
+                                $un = $_SESSION['username'];
+                                $sql = "SELECT * FROM `rb_user_tb` WHERE username='$un'";
+                                $result = mysqli_query($conn, $sql);
+                                while ($row = mysqli_fetch_assoc($result)) {
+                                    echo $row['email'];
+                                }
+                                ?></p>
+        </div>
+    </center>
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    -->
 </body>
 
 </html>

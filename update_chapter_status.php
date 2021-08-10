3fd008f86f690b295300a3d96850d28de20a479b
Anuj Panchal <53812609+AnujP6174@users.noreply.github.com>
2021-08-10 22:23:52 +0530

    init

--- update_chapter_status.php
+++ update_chapter_status.php
@@ -5,11 +5,8 @@ $conn = mysqli_connect("localhost", "root", "", "rbeitest_db");
 $chapter_name = $_GET['chapter'];
 $usr_id = $_GET['user_id'];
 
-// echo "The request was recieved by the ajax call with param chapter name : " . $chapter_name;
-
 $button_query = "INSERT into chapter_completion_tb (user_id,Chap_name,Progress) values('$usr_id','$chapter_name','Done')";
 $button_result = mysqli_query($conn, $button_query) or die(mysqli_error($conn));
-// $button_row = mysqli_fetch_array($button_result);
 ?>
 <!DOCTYPE html>
 <html lang="en">

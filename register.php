c87f047a8471ef8f2b84874329703f2ee68d08fd
Anuj Panchal <53812609+AnujP6174@users.noreply.github.com>
2021-08-29 16:32:08 +0530

    init

--- register.php
+++ register.php
@@ -21,7 +21,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     <div class="center">
         <h1>Register</h1>
         <form method="post">
-
             <div class="txt_field">
                 <input type="text" name="name" maxlength="40" onkeypress="return (event.charCode > 64 && event.charCode < 91) || (event.charCode > 96 && event.charCode < 123) || (event.charCode==32) || (event.charCode>47 && event.charCode<58)" required>
                 <span></span>
@@ -37,23 +36,24 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
                 <span></span>
                 <label>Enter Your Mobile No.</label>
             </div>
-
             <div class="txt_field">
                 <input type="pw" name="password" maxlength="10" onkeypress="return (event.charCode > 48 && event.charCode < 58)" required>
                 <span></span>
                 <label>Create Password(Numbers Only)</label>
             </div>
-            <?php
-            $register_select_class_query = "SELECT * FROM `rb_class_tb`";
-            $register_select_class_result = mysqli_query($conn, $register_select_class_query) or die(mysqli_error($conn));
-            echo "<form method='GET'>
+            <div class="dropdown">
+                <?php
+                $register_select_class_query = "SELECT * FROM `rb_class_tb`";
+                $register_select_class_result = mysqli_query($conn, $register_select_class_query) or die(mysqli_error($conn));
+                echo "<form method='GET'>
             <label>Select Your Class: </label>
             <select name='register_class' id='register_class'>";
-            while ($register_select_class_row = mysqli_fetch_array($register_select_class_result)) {
-                echo "<option value='$register_select_class_row[1]'>$register_select_class_row[1]</option>";
-            }
-            echo "</select></form><br>";
-            ?>
+                while ($register_select_class_row = mysqli_fetch_array($register_select_class_result)) {
+                    echo "<option value='$register_select_class_row[1]'>$register_select_class_row[1]</option>";
+                }
+                echo "</select></form><br>";
+                ?>
+            </div>
             <input type="submit" name="signup" value="SignUp">
             <div class="my-4"></div>
         </form>

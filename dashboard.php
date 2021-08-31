53c02ee7a24a667d134284f03fbe3e5864e5edf3
Anuj Panchal <53812609+AnujP6174@users.noreply.github.com>
2021-08-31 21:47:03 +0530

    init

--- dashboard.php
+++ dashboard.php
@@ -18,7 +18,7 @@ $conn = mysqli_connect("localhost", "root", "", "rbeitest_db") or die("Connectio
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="dashboard.css">
-    <title><?php echo ($_SESSION['username']); ?> DashBoard</title>
+    <title><?php echo ($_SESSION['username']); ?>'s DashBoard</title>
     <!-- <script language="javascript" type="text/javascript">
         window.history.forward();
     </script> -->

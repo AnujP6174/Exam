79b8bbb960ea398945e341f6e1f04f21162a4de0
Anuj Panchal <53812609+AnujP6174@users.noreply.github.com>
2021-08-31 20:56:49 +0530

    init

--- about.php
+++ about.php
@@ -14,19 +14,24 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
     <script language="javascript" type="text/javascript">
         window.history.forward();
     </script>
+    <link rel="stylesheet" href="style.css">
     <style>
         body {
             background: linear-gradient(120deg, #2980b9, #8e44ad);
             background-attachment: fixed;
-            font-family: Roboto,sans-serif;
+            font-family: Roboto, sans-serif;
         }
 
         span {
@@ -60,43 +65,47 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         </div>
     </nav>
     <!-- Navbar ends -->
-    <center>
-        <div class="container my-4">
-            <p><b>Name:</b> <?php
-                            $un = $_SESSION['username'];
-                            $sql = "SELECT * FROM `rb_user_tb` WHERE username='$un'";
-                            $result = mysqli_query($conn, $sql);
-                            while ($row = mysqli_fetch_assoc($result)) {
-                                echo $row['name'];
-                            }
-                            ?></p>
-            <p><b>Class:</b> <?php
+
+    <div class="center">
+        <div class="txt_field my-4">
+            <center>
+                <!-- <div class="container my-4"> -->
+                <p><b>Name:</b> <?php
                                 $un = $_SESSION['username'];
                                 $sql = "SELECT * FROM `rb_user_tb` WHERE username='$un'";
                                 $result = mysqli_query($conn, $sql);
                                 while ($row = mysqli_fetch_assoc($result)) {
-                                    echo $row['class'];
+                                    echo $row['name'];
                                 }
                                 ?></p>
-            <p><b>Mobile:</b> <?php
-                                $un = $_SESSION['username'];
-                                $sql = "SELECT * FROM `rb_user_tb` WHERE username='$un'";
-                                $result = mysqli_query($conn, $sql);
-                                while ($row = mysqli_fetch_assoc($result)) {
-                                    echo $row['mobile'];
-                                }
-                                ?></p>
-            <p><b>Email:</b> <?php
-                                $un = $_SESSION['username'];
-                                $sql = "SELECT * FROM `rb_user_tb` WHERE username='$un'";
-                                $result = mysqli_query($conn, $sql);
-                                while ($row = mysqli_fetch_assoc($result)) {
-                                    echo $row['email'];
-                                }
-                                ?></p>
+                <p><b>Class:</b> <?php
+                                    $un = $_SESSION['username'];
+                                    $sql = "SELECT * FROM `rb_user_tb` WHERE username='$un'";
+                                    $result = mysqli_query($conn, $sql);
+                                    while ($row = mysqli_fetch_assoc($result)) {
+                                        echo $row['class'];
+                                    }
+                                    ?></p>
+                <p><b>Mobile:</b> <?php
+                                    $un = $_SESSION['username'];
+                                    $sql = "SELECT * FROM `rb_user_tb` WHERE username='$un'";
+                                    $result = mysqli_query($conn, $sql);
+                                    while ($row = mysqli_fetch_assoc($result)) {
+                                        echo $row['mobile'];
+                                    }
+                                    ?></p>
+                <p><b>Email:</b> <?php
+                                    $un = $_SESSION['username'];
+                                    $sql = "SELECT * FROM `rb_user_tb` WHERE username='$un'";
+                                    $result = mysqli_query($conn, $sql);
+                                    while ($row = mysqli_fetch_assoc($result)) {
+                                        echo $row['email'];
+                                    }
+                                    ?></p>
+            </center>
         </div>
-    </center>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    </div>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> -->
 </body>
 
 </html>

4c259428ea2107c8c3204a053513f7d7b6ee2373
Anuj Panchal <53812609+AnujP6174@users.noreply.github.com>
2021-09-15 19:56:50 +0530

    init

--- login.php
+++ login.php
@@ -1,10 +1,3 @@
-<?php
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  session_start();
-  $_SESSION['logged'] = 'yes';
-  $conn = mysqli_connect("localhost", "root", "", "rbeitest_db") or die("Connection Failed");
-}
-?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -15,7 +8,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   <title>RBeI Log-in</title>
-  
+
   <!-- <script language="javascript" type="text/javascript">
     window.history.forward();
   </script> -->
--- logout.php
+++ logout.php
@@ -5,7 +5,7 @@ unset($_SESSION["id"]);
 unset($_SESSION["class"]);
 session_destroy();
 session_write_close();
-header('Location: /internship/login');
+header('Location: login');
 exit();
 ?>
 <!DOCTYPE html>

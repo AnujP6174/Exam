45a66f04c67576c81290b224febaf539151d33a3
Anuj Panchal <53812609+AnujP6174@users.noreply.github.com>
2021-11-03 08:31:47 +0530

    init

--- captcha.php
+++ captcha.php
@@ -1,12 +1,12 @@
 <?php
-    session_start();
-    $random_number=rand(11111,99999);
-    $_SESSION['CODE']=$random_number;
-    $layer=imagecreatetruecolor(60,30);
-    $captcha_bg=imagecolorallocate($layer,147,71,66);
-    imagefill($layer,0,0,$captcha_bg);
-    $captcha_text_color=imagecolorallocate($layer,0,0,0);
-    imagestring($layer,5,5,5,$random_number,$captcha_text_color);
-    header('Content-Type:image/jpeg');
-    imagejpeg($layer);
+session_start();
+$random_number = rand(11111, 99999);
+$_SESSION['CODE'] = $random_number;
+$layer = imagecreatetruecolor(60, 30);
+$captcha_bg = imagecolorallocate($layer, 147, 71, 66);
+imagefill($layer, 0, 0, $captcha_bg);
+$captcha_text_color = imagecolorallocate($layer, 0, 0, 0);
+imagestring($layer, 5, 5, 5, $random_number, $captcha_text_color);
+header('Content-Type:image/jpeg');
+imagejpeg($layer);
 ?>
